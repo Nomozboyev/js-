2f3c47026da471649a92a5fb0ecedb6c5f38b2d1
Nomozboyev <husniddinbeknomozboyev@gmail.com>
2023-03-11 15:07:58 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var name = prompt( "what is your name?");
+console.log(name) ; 
+var surname = prompt( "what is your last name");
+console.log("surname");
+var age = prompt ("how old are you");
+console.log(age) ;
+var work = prompt ("what is your profession");
+var voyaga = confirm("are you an adult ?");
+console.log(voyaga)
+var warning= confirm("agree to the terms of the site")
+var welcome="welcome to our site"
+console.log(welcome)
+console.log(name,surname,age,work,voyaga,warning)
